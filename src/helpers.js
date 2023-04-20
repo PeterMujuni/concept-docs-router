6f2e40060d39a2193bde69ac0144c491fd31ee58
Peter Mujuni <kontakt@petermujuni.dk>
2023-04-20 13:18:26 +0200

    budget and expense forms plus format added

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,62 @@
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+
+// color
+const generateRandomColor = () => {
+	const existingBudgetLength = fetchData("budgets")?.length ?? 0
+	return `${existingBudgetLength * 34} 65% 50%`
+}
+
 // Local storage
 export const fetchData = (key) => {
 	return JSON.parse(localStorage.getItem(key));
 };
 
+// create budget
+export const createBudget = ({ name, amount }) => {
+	const newItem = {
+		id: crypto.randomUUID(),
+		name: name,
+		createdAt: Date.now(),
+		amount: +amount,
+		color: generateRandomColor()
+	};
+
+	const existingBudgets = fetchData("budgets") ?? [];
+	return localStorage.setItem(
+		"budgets",
+		JSON.stringify([...existingBudgets, newItem])
+	);
+};
+
+// create expense
+export const createExpense = ({ name, amount, budgetId }) => {
+	const newItem = {
+		id: crypto.randomUUID(),
+		name: name,
+		createdAt: Date.now(),
+		amount: +amount,
+		budgetId: budgetId,
+	};
+
+	const existingExpenses = fetchData("expenses") ?? [];
+	return localStorage.setItem(
+		"expenses",
+		JSON.stringify([...existingExpenses, newItem])
+	);
+};
+
 // delete item
-export const deleteItem = ({key}) => {
-	return localStorage.removeItem(key)
+export const deleteItem = ({ key }) => {
+	return localStorage.removeItem(key);
+};
+
+
+// FORMATTING
+
+// Format currency
+export const formatCurrency = (amt) => {
+	return amt.toLocaleString("da-DK", {
+		style: "currency",
+		currency: "DKK",
+	});
 }
